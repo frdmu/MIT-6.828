da926afb0182654d132b342471d263606c435042
frdmu <2320899425@qq.com>
2021-04-04 12:08:23 -0400

    lab4

--- code/inc/error.h
+++ code/inc/error.h
@@ -13,7 +13,8 @@ enum {
 	E_NO_FREE_ENV	,	// Attempt to create a new environment beyond
 				// the maximum allowed
 	E_FAULT		,	// Memory fault
-
+	E_IPC_NOT_RECV	,	// Attempt to send to env that is not recving
+	E_EOF		,	// Unexpected end of file
 	MAXERROR
 };
 
