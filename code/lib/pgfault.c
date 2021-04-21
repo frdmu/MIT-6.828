3e7219657c6dda15f33a4a9fbb2a5ac079274918
frdmu <2320899425@qq.com>
2021-04-21 00:47:42 -0400

    lab4 exercise 11 done

--- code/lib/pgfault.c
+++ code/lib/pgfault.c
@@ -29,7 +29,10 @@ set_pgfault_handler(void (*handler)(struct UTrapframe *utf))
 	if (_pgfault_handler == 0) {
 		// First time through!
 		// LAB 4: Your code here.
-		panic("set_pgfault_handler not implemented");
+		envid_t envid = sys_getenvid();
+		sys_page_alloc(envid, (void*)(UXSTACKTOP - PGSIZE), PTE_W | PTE_U | PTE_P);
+		sys_env_set_pgfault_upcall(envid, _pgfault_upcall);	
+		//panic("set_pgfault_handler not implemented");
 	}
 
 	// Save handler pointer for assembly to call.
