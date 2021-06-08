0a5341754d2396e35ad18b16a521043b4d016ecf
frdmu <2320899425@qq.com>
2021-06-08 00:14:50 -0400

    lab6 exercise 4 done

--- code/kern/e1000.h
+++ code/kern/e1000.h
@@ -1,7 +1,8 @@
 #ifndef JOS_KERN_E1000_H
+#include <inc/types.h>
 #define PCI_E1000_VENDOR_ID 0x8086
 #define PCI_E1000_DEVICE_ID 0x100e
-
+volatile uint32_t * e1000;
 
 #define JOS_KERN_E1000_H
 #endif  // SOL >= 6
