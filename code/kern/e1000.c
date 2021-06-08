23c53c9a7e8928ea64f7fab6c51100c07bdd50ce
frdmu <2320899425@qq.com>
2021-06-08 10:03:49 -0400

    lab6 exercise 5 done

--- code/kern/e1000.c
+++ code/kern/e1000.c
@@ -1,11 +1,29 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
 #include <kern/pci.h>
+#include <inc/string.h>
 // LAB 6: Your driver code here
+void e1000_transmit_init() {
+    int i; 
+    // Initialize transmit Descriptor list entries to 0 
+    memset(e1000_tx_desc_array, 0, sizeof(struct e1000_tx_desc) * E1000_TX_DESC_ARRAY_SIZE);
+    for (i = 0; i < E1000_TX_DESC_ARRAY_SIZE; i++) {
+        e1000_tx_desc_array[i].addr = PADDR(e1000_tx_packet_buf[i]);
+        e1000_tx_desc_array[i].status = E1000_TXD_STAT_DD;
+    }
+    
+    e1000[LOCATION(E1000_TDBAL)] = PADDR(e1000_tx_desc_array);
+    e1000[LOCATION(E1000_TDLEN)] = sizeof(struct e1000_tx_desc) * E1000_TX_DESC_ARRAY_SIZE;
+    e1000[LOCATION(E1000_TDH)] = 0;
+    e1000[LOCATION(E1000_TDT)] = 0;
+    e1000[LOCATION(E1000_TCTL)] = E1000_TCTL_EN | E1000_TCTL_PSP | (E1000_TCTL_CT & (0x10 << 4)) | (E1000_TCTL_COLD & (0x40 << 12));
+    e1000[LOCATION(E1000_TIPG)] = 10 | (4 << 10) | (6 << 20);
+}
 int 
 pci_e1000_attach(struct pci_func *pcif) {
     pci_func_enable(pcif);
     e1000 = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]); 
-    cprintf("device status:[%08x]\n", e1000[2]); 
+    cprintf("device status:[%08x]\n", e1000[LOCATION(E1000_STATUS)]); 
+    e1000_transmit_init();
     return 0;
 }
--- code/kern/e1000.h
+++ code/kern/e1000.h
@@ -1,8 +1,49 @@
 #ifndef JOS_KERN_E1000_H
 #include <inc/types.h>
+#include <inc/mmu.h>
+
 #define PCI_E1000_VENDOR_ID 0x8086
 #define PCI_E1000_DEVICE_ID 0x100e
+#define LOCATION(offset) (offset >> 2)
 volatile uint32_t * e1000;
+
+/* Register Set */
+#define E1000_STATUS   0x00008  /* Device Status - RO */
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+
+/* Transmit Control bit definitions */
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
+#define E1000_TCTL_CT     0x00000ff0    /* collision threshold */
+#define E1000_TCTL_COLD   0x003ff000    /* collision distance */
+
+/* Transmit Descriptor*/
+struct e1000_tx_desc
+{
+	uint64_t addr;   /* Address of packet buffer*/
+	uint16_t length; /* Data buffer length */
+	uint8_t cso;     /* Checksum offset */
+	uint8_t cmd;     /* Descriptor control */
+	uint8_t status;  /* Descriptor status */
+	uint8_t css;     /* Checksum start */
+	uint16_t special;
+}__attribute__((packed));
+/* Transmit Descriptor bit definitions */
+#define E1000_TXD_STAT_DD    0x00000001 /* Descriptor Done */
+
+/* Reserve memory for the transmit descriptor array and the packet buffers pointed to by the transmit descriptors */
+#define E1000_TX_DESC_ARRAY_SIZE 64 /* Size of transmit descriptor queue */
+#define E1000_TX_DESC_BUF_MTU 1518  /* The maximum size of an Ethernet packet */
+struct e1000_tx_desc e1000_tx_desc_array[E1000_TX_DESC_ARRAY_SIZE] __attribute__((aligned(PGSIZE))); 
+char e1000_tx_packet_buf[E1000_TX_DESC_ARRAY_SIZE][E1000_TX_DESC_BUF_MTU] __attribute__((aligned(PGSIZE))); 
+
+/* Func */
+void e1000_transmit_init(); 
 
 #define JOS_KERN_E1000_H
 #endif  // SOL >= 6
